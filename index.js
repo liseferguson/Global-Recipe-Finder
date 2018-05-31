8debfbaf24c8d897293bad2b8b064add176cfb0f
Lise Ferguson <lise.ferguson503@gmail.com>
2018-05-30 18:53:00 -0700

    added more css, more html, and javascript as well as images and API data

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-'use strict'
+'use strict';
+
+const WIKIPEDIA_SEARCH_URL = 'https://en.wikipedia.org/w/api.php';
+
+function getDataFromApi(searchTerm, callback) {
+  const settings = {
+$.ajax( {
+    url: remoteUrlWithOrigin,
+    data: queryData,
+    dataType: 'json',
+    type: 'POST',
+    headers: { 'Api-User-Agent': 'Example/1.0' },
+    success: callback(data) {
+       // do something with data
+    }
+} );
+$.ajax(settings);
+}
+
+function handleRecipeButton(){
+  $('.js-recipe-button').click(function(event){
+//when user clicks button, country appears along with flag, map, and recipe (and hopefully photo). Text on button changes to "Try another coutnry" and will generate new country etc if clicked again
+  });
+}
+
+function handleSubmitButton() {
+  $('.submit').click(function (event){
+//when user submits zipcode, returns restaurants nearby that serve food from country currently selected in .country-text
+  });
+}
