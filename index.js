512cdff56e638005619ed305cf7e2b7e4d2dca04
Lise Ferguson <lise.ferguson503@gmail.com>
2018-05-30 23:36:15 -0700

    changed project scope, added data and random selection functions

--- index.js
+++ index.js
@@ -1,30 +1,46 @@
 'use strict';
 
-const WIKIPEDIA_SEARCH_URL = 'https://en.wikipedia.org/w/api.php';
+const = YUMMLY_SEARCH_URL='';
 
+const = YELP_SEARCG_URL='';
+
+//function will randomly select one of the cuisines in the array
+function selectRandomCuisine(){
+  const cuisines = [
+  "American", "Italian", "Asian", "Mexican", "Southern & Soul Food", "French", "Southwestern", "Barbecue", "Indian", "Chinese", "Cajun & Creole", "English", "Mediterranean", "Greek", "Spanish", "German", "Thai", "Moroccan", "Irish", "Japanese", "Cuban", "Hawaiin", "Swedish", "Hungarian", "Portugese"];
+  let randomCuisine = cuisines[Math.floor(Math.random() * cuisines.length)];
+  console.log(randomCuisine);
+}:
+}
+selectRandomCuisine();
+
+function selectRandomRecipe(){
+  //this will use the same method in selectRandomCuisine to select a random recipe from Yummly in the matching category
+}
+
+//have to change this for Yummly not Wikipedia
 function getDataFromApi(searchTerm, callback) {
   const settings = {
-$.ajax( {
-    url: remoteUrlWithOrigin,
-    data: queryData,
-    dataType: 'json',
-    type: 'POST',
-    headers: { 'Api-User-Agent': 'Example/1.0' },
-    success: callback(data) {
-       // do something with data
-    }
-} );
+      url: "https://en.wikipedia.org/wiki/National_dish#By_country&callback=?",
+      data: queryData,
+      dataType: 'json',
+      type: 'GET',
+      headers: { 'Api-User-Agent': 'Example/1.0' },
+      success: function (data) {
+       console.log(data);
+    };
 $.ajax(settings);
 }
 
 function handleRecipeButton(){
   $('.js-recipe-button').click(function(event){
-//when user clicks button, country appears along with flag, map, and recipe (and hopefully photo). Text on button changes to "Try another coutnry" and will generate new country etc if clicked again
+
+//when user clicks button, cuisine appears along with recipe (and hopefully photo). Text on button changes to "Try another cuisine" and will start the process all over
   });
 }
 
 function handleSubmitButton() {
   $('.submit').click(function (event){
-//when user submits zipcode, returns restaurants nearby that serve food from country currently selected in .country-text
+//when user submits zipcode, returns restaurants nearby that serve food from current cuisine in app
   });
 }
