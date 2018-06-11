633136ec689d194adf57a5bf1d2fdb11ac848e36
Lise Ferguson <lise.ferguson503@gmail.com>
2018-06-11 15:52:37 -0700

    added function rendering recipe info to browser, added foursquare api function

--- index.js
+++ index.js
@@ -2,6 +2,8 @@
 
 let currentCuisine = '';
 
+let userLocation = '';
+
 const YUMMLY_SEARCH_URL = 'https://api.yummly.com/v1';
 
 //function will randomly select one of the cuisines in the array. These cuisines are all the "supported cuisines" for the allowedCuisine parameter
@@ -30,48 +32,89 @@ function setupRecipeButton(){
 }
 
 //retrieves data from API, including pics and matching the current randomly selected cuisine
-function getDataFromYummlyApi(searchTerm, callback) {
+//change click handler to handle click handlers, then call get data from apis inside
+function getDataFromYummlyApi() {
   console.log("setting up the button to get data");
   $('.js-recipe-button').click(function(event){
+  getDataFromFourSquareApi();
   const yumSettings = {
       url: `https://api.yummly.com/v1/api/recipes?_app_id=8dc09775&_app_key=e1e1a4f3182110165850285dd6044a66&requirePictures=true&allowedCuisine[]=cuisine^cuisine-${currentCuisine}`,
       dataType: 'json',
       type: 'GET',
       headers: {},
       success: function (yumData) {
-       console.log(yumData);
+        renderResults(yumData.matches);
+       console.log(yumData.matches[0]);
       },
       error: function(error){
         console.log('oh god oh god oh god oh god');
         console.log(error);
       }
   };
-  console.log('now I have sent the request')
+  //console.log('now I have sent the request')
   $.ajax(yumSettings);
-//  $('.restaurant-form').css('display', 'block');
+$('.restaurant-form').css('display', 'block');
 })
 }
 
-// renders 1 recipe and photo to the browser. imageUrlsBySize is the name of the photos
-function renderResults(matches){
-  return `
-  <div class = "renderedRecipe">
-    <h2>${matches.recipeName}</h2>
-    <img src="https://api.yummly.com/v1/api/recipes?_app_id=8dc09775&_app_key=e1e1a4f3182110165850285dd6044a66&requirePictures=true&allowedCuisine[]=cuisine^cuisine-${currentCuisine}${matches.smallImageUrls}" role= "image" alt= "Recipe photo">
-    <h3>${matches.ingredients}</h3>
-  </div>
-    `
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        userLocation.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+function showPosition(position) {
+    userLocation.innerHTML = "Latitude: " + position.coords.latitude +
+    "<br>Longitude: " + position.coords.longitude;
 }
 
-//this function makes recipe and photo appear on page in <div class="js-current-recipe">
-function displayYummlySearchData(yumData) {
-  const results = yumData.matches.map((matches, recipeName) => renderResults(recipeName));
-  $('.js-current-recipe').html(results);
+function getDataFromFourSquareApi() {
+  console.log("setting up the button to get data");
+  const fourSquareSettings = {
+    data: {
+    client_id: 'CTZMWEX0B0B0G4CORJDSKDOC2N2K0HNFLWC1AXMUQ4CBJZ2S',
+    client_secret: 'RDP3OYDZ0TPNATBZY2XA3TUHV3LZFX23OQ4PI3LEHLVQ5E1J',
+    ll: '45.5481005, -122.5849967',
+    query: currentCuisine,
+    v: '20180323',
+    limit: 1
+  },
+      url: `https://api.foursquare.com/v2/venues/explore`,
+      dataType: 'json',
+      type: 'GET',
+      success: function (fourSquareData) {
+       console.log(fourSquareData);
+      },
+      error: function(error){
+        console.log('oh god oh god oh god oh god');
+        console.log(error);
+      }
+  };
+  //console.log('now I have sent the request')
+  $.ajax(fourSquareSettings);
+//  $('.restaurant-form').css('display', 'block');
+
+}
+
+function renderResults(matches){
+  let bigString = renderSingleRecipe(matches[Math.floor(Math.random() * matches.length)]);
+  $(".js-current-recipe").html(bigString);
+}
+
+function renderSingleRecipe(recipe) {
+  console.log(recipe.smallImageUrls[0]);
+  return (`
+  <div class = "renderedRecipe">
+    <h2>${recipe.recipeName}</h2>
+    <a href="https://www.yummly.com/recipe/${recipe.id}" target="new">Click here for recipe</a>
+    <img src="${recipe.smallImageUrls[0]}" role= "image" alt= "Recipe photo">
+  </div>`)
+
 }
 
 $(function(){
 setupRecipeButton();
 getDataFromYummlyApi();
-renderResults();
-displayYummlySearchData();
+//displayYummlySearchData();
 });
