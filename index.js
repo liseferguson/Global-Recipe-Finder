4fb0b5196d05cfd2678d7d123d45b5168999f9eb
Lise Ferguson <lise.ferguson503@gmail.com>
2018-06-20 13:37:31 -0700

    removed 2nd API, linked to yelp

--- index.js
+++ index.js
@@ -2,16 +2,12 @@
 
 let currentCuisine = '';
 
-let userLocation = '';
-
-const YUMMLY_SEARCH_URL = 'https://api.yummly.com/v1';
-
 //function will randomly select one of the cuisines in the array. These cuisines are all the "supported cuisines" for the allowedCuisine parameter
 function selectRandomCuisine() {
   const cuisines = [
-    "american", "italian", "asian", "mexican", "southern & soul food",
+    "american", "italian", "asian", "mexican", "southern",
     "french", "southwestern", "barbecue", "indian", "chinese",
-    "cajun & creole", "english", "mediterranean", "greek", "spanish",
+    "cajun", "english", "mediterranean", "greek", "spanish",
     "german", "thai", "moroccan", "irish", "japanese",
     "cuban", "hawaiian", "swedish", "hungarian", "portugese"
   ];
@@ -26,17 +22,15 @@ function setupRecipeButton(){
   console.log('setting up the click handler')
   $('.js-recipe-button').click(function(event){
     selectRandomCuisine()
-    $('.cuisine-text').html(`<div>${currentCuisine}</div>`)
+    $('.cuisine-text').html(`<div>${currentCuisine}!</div>`)
     $('.js-recipe-button').text("Try another cuisine!");
   });
 }
 
 //retrieves data from API, including pics and matching the current randomly selected cuisine
-//change click handler to handle click handlers, then call get data from apis inside
 function getDataFromYummlyApi() {
   console.log("setting up the button to get data");
   $('.js-recipe-button').click(function(event){
-  getDataFromFourSquareApi();
   const yumSettings = {
       url: `https://api.yummly.com/v1/api/recipes?_app_id=8dc09775&_app_key=e1e1a4f3182110165850285dd6044a66&requirePictures=true&allowedCuisine[]=cuisine^cuisine-${currentCuisine}`,
       dataType: 'json',
@@ -57,78 +51,28 @@ $('.restaurant-form').css('display', 'block');
 })
 }
 
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        userLocation.innerHTML = "Geolocation is not supported by this browser.";
-    }
-}
-function showPosition(position) {
-    userLocation.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
-}
-
-function getDataFromFourSquareApi() {
-  console.log("setting up the button to get data");
-  const fourSquareSettings = {
-    data: {
-    client_id: 'CTZMWEX0B0B0G4CORJDSKDOC2N2K0HNFLWC1AXMUQ4CBJZ2S',
-    client_secret: 'RDP3OYDZ0TPNATBZY2XA3TUHV3LZFX23OQ4PI3LEHLVQ5E1J',
-    ll: userLocation,
-    query: currentCuisine,
-    v: '20180323',
-    limit: 1
-  },
-      url: `https://api.foursquare.com/v2/venues/explore`,
-      dataType: 'json',
-      type: 'GET',
-      success: function (fourSquareData) {
-       console.log(fourSquareData);
-      },
-      error: function(error){
-        console.log('oh god oh god oh god oh god');
-        console.log(error);
-      }
-  };
-  //console.log('now I have sent the request')
-  $.ajax(fourSquareSettings);
-$('.restaurant-form').css('display', 'block');
-
-}
-
 function renderResults(matches){
   let bigString = renderSingleRecipe(matches[Math.floor(Math.random() * matches.length)]);
-  $(".js-current-recipe").html(bigString);
+  $(".recipe-text").html(bigString);
 }
 
 function renderSingleRecipe(recipe) {
-  //console.log(recipe.smallImageUrls[0]);
   return (`
   <div class = "renderedRecipe">
     <h2>${recipe.recipeName}</h2>
+    <img src="${recipe.smallImageUrls[0]}" class= "recipeImage" role= "image" alt= "Recipe photo">
     <a href="https://www.yummly.com/recipe/${recipe.id}" target="new">Click here for recipe</a>
-    <img src="${recipe.smallImageUrls[0]}" role= "image" alt= "Recipe photo">
   </div>`)
-
 }
 
-function renderVenueResults(results){
-  let bigString = renderSingleVenue(results[Math.floor(Math.random() * matches.length)]);
-  $(".js-current-venue").html(bigString);
-}
-
-function renderSingleVenue(venue) {
-  console.log(totalResults)
-  return (`
-  <div class = "renderedVenue">
-    <h2>${venue.name}</h2>
-    <h3>${venue.location.address}</h2>
-    <a href="" target="new">Click here to visit restaurant site</a>`)
-}
+$('.userZipcode').submit((event) =>{
+  event.preventDefault()
+  let targetzip = $('.zip-input').val()
+  let targetURL = `https://www.yelp.com/search?find_desc=${currentCuisine}&find_loc=${targetzip}`
+  window.open(targetURL, '_blank');
+})
 
 $(function(){
-getLocation();
 setupRecipeButton();
 getDataFromYummlyApi();
 //displayYummlySearchData();
